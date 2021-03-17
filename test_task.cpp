a6a9e48130f2b53acef78b06f80ec65b4edce5ba
freeeyes <freeeyes@163.com>
2021-03-17 08:58:27 +0800

    add test sonar

--- test_task.cpp
+++ test_task.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+/*
 #include <gtest/gtest.h>
 
 TEST(run_test_iotoio, test_iotoio)
@@ -6,12 +7,23 @@ TEST(run_test_iotoio, test_iotoio)
 	int a = 1 + 2;
     EXPECT_EQ(a, 3);
 }
+*/
+
+int sum(int a, int b)
+{
+	return a+b;
+}
 
 int main(int argc, char* argv[])
 {
     //‘À––google test ≤‚ ‘”√¿˝
+	/*
     ::testing::GTEST_FLAG(output) = "xml:Test_add.xml";
     ::testing::InitGoogleTest(&argc, argv);
 
     return RUN_ALL_TESTS();
+	*/
+	auto a = sum(1, 2);
+	
+	return 0;
 }
